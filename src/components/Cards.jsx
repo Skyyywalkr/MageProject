337feb956c4c6418d6b7888acc396d16c0216c80
skyyywalkr <rafay9641@gmail.com>
2020-12-19 23:34:03 +0500

    completed

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,61 +1,154 @@
-import React from 'react'
-import {useState,useEffect} from 'react'
-import axios from 'axios'
-import SearchBox from './Search-Box'
-import './card-style.css'
-import './Card-list.css'
-
-
-
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import SearchBox from "./Search-Box";
+import "./card-style.css";
+import "./Card-list.css";
 
 export default function Cards() {
+  const [alldata, setAllData] = useState([]);
+  const [data, setdata] = useState([]);
 
-    const [data, setdata] = useState([]);
-    const [ searchField, setsearchField] = useState('');
-    
-
-  useEffect( ()=> {
-
-    axios.get('https://www.hatchways.io/api/assessment/students')
-    .then(function (response) {
+  useEffect(() => {
+    console.log("value");
+    axios
+      .get("https://www.hatchways.io/api/assessment/students")
+      .then(function (response) {
         console.log(response);
-      setdata(response.data.students)
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
+        let arr = response.data.students;
+        if (arr.length > 0) {
+          for (let index = 0; index < arr.length; index++) {
+            arr[index]["isShow"] = false;
+            arr[index]["tags"] = [];
+          }
+        }
+        setdata(response.data.students);
+        setAllData(response.data.students);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }, []);
 
-  }
+  //
+
+  const filterdata = (value) => {
+    console.log(value);
+    if (value) {
+      setdata(
+        data.filter((data) =>
+        data.firstName.toLowerCase().includes(value.toLowerCase())
+        )
+      );
+    } else {
+      setdata(alldata);
+    }
+  };
+  const filterdataByTag = (value) => {
+    console.log(value);
+    if (value) {
+      let dataContainer = [];
+    for (let index = 0; index < data.length; index++) {
+      const element = data[index];
+        
+        let ele = element.tags.filter((data) =>data.toLowerCase().includes(value.toLowerCase()))
+
+        if(ele.length > 0){
+
+          dataContainer.push(element)
+        }
+
+    }
+      setdata(dataContainer);
+    }else{
+      setdata(alldata);
+    }
+    
+  };
+  const showDetail = (index) => {
+    data[index]["isShow"] = !data[index]["isShow"];
+
+    setdata([...data]);
+  };
+  const saveTag = (name , index) => {
+    data[index]["tags"].push(name);
+    setdata([...data]);
+  };
+
+  const _handleKeyDown = (e , index) => {
+    if (e.key === 'Enter' && e.target.value) {
+      saveTag(e.target.value , index)
+      e.target.value = '';
+    }
+  } 
+
+  return (
+    <>
+      <SearchBox
+        placeHolder="Search by name"
+        handleChange={(e) => {
+          filterdata(e.target.value);
+        }}
+      />
+      <SearchBox
+        placeHolder="Search by tag"
+        handleChange={(e) => {
+          filterdataByTag(e.target.value);
+        }}
+      />
+      <div className={"CardList"}>
+        {data.map((item, key) => (
+          <div key={key} className={"card-container student"}>
+            <div className="buttonHolder">
+              <button onClick={() => showDetail(key)}>
+                {item.isShow ? "-" : "+"}
+              </button>
+            </div>
+            <div>
+              <img className="avatar" alt="Monster" src={item.pic} />
+            </div>
+            <h3>
+              {item.firstName} {item.lastName}
+            </h3>
+            <span> Company: {item.company}</span>
+            <span> City: {item.city}</span>
+            <span>Email: {item.email}</span>
+
+            <span>Skill: {item.skill}</span>
+
+            <span>
+              Average:{" "}
+              {item.grades.reduce((a, b) => +a + +b, 0) / item.grades.length}%
+            </span>
+
+            {item["isShow"]
+              ? <div>
+
+                {item.grades.map((x, i) => (
+                  <div>
+                    <div key={i}>
+                      Test {i}: {x} %
+                    </div>
+                  </div>
+                  ))}
+
+                  {item["tags"].map((tag, i) => (
+                  <div>
+                    <p className="tag"  key={i}>{tag}</p>
+                   
+                  </div>
+
+                  ))}
+
+                  <input type="text" className="add-tag" placeholder="Add a Tag"  onKeyDown={(e)=> _handleKeyDown(e , key)}/>
+
+              </div> 
+
+                : ""}
+          </div>
+        ))}
+      </div>
+    </>
   );
-
-//
-
-const filterdata = data.filter(
-    data => data.city.toLowerCase().includes(searchField.toLowerCase())
-    );
-
-    return (
-        <>
-        <SearchBox  
-        placeHolder='Search Monster'  
-        handleChange ={e => setdata({searchField : e.target.value})}/>
-        <div className={'CardList'}>
-           
-           
-            {
-                data.map(data1 => (
-                    <div className={'card-container'}>
-                   <div><img alt='Monster' src={data1.pic}/></div>
-                    <div>{data1.city}</div>
-                      </div> 
-                    )
-                )
-            }
-            
-        </div>
-</>
-    )
 }
--- src/components/Search-Box.jsx
+++ src/components/Search-Box.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function SearchBox({ placeHolder , handleChange}) {
     return (
-        <div>
+        <div className="search-center">
 
-<input type='search' 
+<input className="search" type='search' 
         placeholder={placeHolder} 
         onChange={handleChange}/>
             
